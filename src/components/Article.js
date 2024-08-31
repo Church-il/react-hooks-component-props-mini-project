93b9639e646cd86cda3f1f4a3ae05ca2ec2c91ae
Michael Akoko <akokomichael37@gmail.com>
2024-08-31 16:28:09 +0300

    include new files

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,20 @@
 import React from "react";
 
 function Article({ title, date = "January 1, 1970", preview, minutes }) {
+  // Function to generate the minutes to read indicator
+  const generateReadTime = (minutes) => {
+    const emoji = minutes < 30 ? "☕️" : "🍱";
+    const interval = minutes < 30 ? 5 : 10;
+    const emojiCount = Math.ceil(minutes / interval);
+
+    return `${emoji.repeat(emojiCount)} ${minutes} min read`;
+  };
+
   return (
     <article>
       <h3>{title}</h3>
-      <small>{date}</small> {/* Display the date */}
+      <small>{date}</small>
+      <p>{generateReadTime(minutes)}</p> {/* Display minutes to read */}
       <p>{preview}</p>
     </article>
   );
